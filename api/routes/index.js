b4619395d837c4e6a25bdcb2b48c0138970360c0
Alberto <albertojuliogarciafernandez@alumno.ieselrincon.es>
2023-10-20 20:32:43 +0100

    index.js file in routes folder, modified

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-router.use('/actor', require('./actor.router.js'));
-router.use('/address', require('./address.router.js'));
-router.use('/country', require('./country.router.js'));
-router.use('/movie', require('./movie.router.js'));
+router.use('/booking', require('./booking.router.js'));
+router.use('/building', require('./building.router.js'));
+router.use('/classroom', require('./classroom.router.js'));
+router.use('/equipment', require('./equipment.router.js'));
 router.use('/user', require('./user.router.js'));
 
 module.exports = router;
