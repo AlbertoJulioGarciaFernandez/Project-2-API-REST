0ed8044e08edfe44e63cc2ad844d3efdfe17701f
Alberto <ajgf1984@gmail.com>
2023-10-23 10:21:11 +0100

    Updated index.js in routes folder (removing commentary)

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 // router.use('/booking', require('./booking.router.js'));
-// router.use('/building', require('./building.router.js'));
+router.use('/building', require('./building.router.js'));
 // router.use('/classroom', require('./classroom.router.js'));
 // router.use('/equipment', require('./equipment.router.js'));
 router.use('/user', require('./user.router.js'));
