a86914bd0608744fe960a4dc9e432292cd6e0fd8
aitor <aitorsancab@gmail.com>
2023-10-23 10:42:44 +0100

    slq classrooms y equipments

---
+++ mysql/classroom.sql
@@ -0,0 +1,37 @@
+INSERT INTO 'classrooms' ('id', 'classroomName', 'capacity', 'idBuilding', 'aimedAt') VALUES
+(1, 'Aula 101', 50, 1, 'professor'),
+(2, 'Aula 102', 40, 1, 'student'),
+(3, 'Aula 103', 60, 1, 'professor'),
+(4, 'Aula 104', 45, 1, 'student'),
+(5, 'Aula 201', 60, 2, 'professor'),
+(6, 'Aula 202', 45, 2, 'student'),
+(7, 'Aula 203', 70, 2, 'professor'),
+(8, 'Aula 204', 55, 2, 'student'),
+(9, 'Aula 301', 70, 3, 'professor'),
+(10, 'Aula 302', 55, 3, 'student'),
+(11, 'Aula 303', 70, 3, 'professor'),
+(12, 'Aula 304', 55, 3, 'student'),
+(13, 'Aula 401', 50, 4, 'professor'),
+(14, 'Aula 402', 40, 4, 'student'),
+(15, 'Aula 403', 50, 4, 'professor'),
+(16, 'Aula 404', 40, 4, 'student'),
+(17, 'Aula 501', 60, 5, 'professor'),
+(18, 'Aula 502', 45, 5, 'student'),
+(19, 'Aula 503', 60, 5, 'professor'),
+(20, 'Aula 504', 45, 5, 'student'),
+(21, 'Aula 601', 70, 6, 'professor'),
+(22, 'Aula 602', 55, 6, 'student'),
+(23, 'Aula 601', 70, 6, 'professor'),
+(24, 'Aula 602', 55, 6, 'student'),
+(25, 'Aula 701', 50, 7, 'professor'),
+(26, 'Aula 702', 40, 7, 'student'),
+(27, 'Aula 703', 50, 7, 'professor'),
+(28, 'Aula 704', 40, 7, 'student');
+
+
+
+
+
+
+
+
