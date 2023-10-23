dd0d4de92fc766ea167e9a0e74197c9b67245d54
Alberto J <47208540+AlbertoJulioGarciaFernandez@users.noreply.github.com>
2023-10-23 09:39:49 +0100

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,11 +2,16 @@ const User = require('../models/user.model.js')
 
 async function getAllUsers(req, res) {
 	try {
-		const users = await User.findAll({ paranoid: false })
+		const users = await User.findAll({
+			// Filtering with the «where» clause in case query 
+			// params are passed as arguments.
+			where: req.query,
+			paranoid: false
+		})
 		if (users) {
-			return res.status(200).json(users)
+			return res.status(200).json({ users: users })
 		} else {
-			return res.status(404).send('No users found')
+			return res.status(404).send('No users found.')
 		}
 	} catch (error) {
 		res.status(500).send(error.message)
@@ -17,9 +22,9 @@ async function getOneUser(req, res) {
 	try {
 		const user = await User.findByPk(req.params.id)
 		if (user) {
-			return res.status(200).json(user)
+			return res.status(200).json({ user: user })
 		} else {
-			return res.status(404).send('User not found')
+			return res.status(404).send('User not found.')
 		}
 	} catch (error) {
 		res.status(500).send(error.message)
@@ -27,12 +32,11 @@ async function getOneUser(req, res) {
 }
 
 async function createUser(req, res) {
-	console.log(req.body);
 	try {
 		const user = await User.create(
 			req.body
 		)
-		return res.status(200).json({ message: 'User created', user: user })
+		return res.status(200).json({ message: 'User successfully created!', user: user })
 	} catch (error) {
 		res.status(500).send(error.message)
 	}
@@ -40,17 +44,30 @@ async function createUser(req, res) {
 
 async function updateUser(req, res) {
 	try {
-		const [userExist, user] = await User.update(req.body, {
-			returning: true,
-			where: {
-				id: req.params.id,
-			},
-		})
-		if (userExist !== 0) {
-			return res.status(200).json({ message: 'User updated', user: user })
+		// Before trying to update the user, we have to
+		// check in the database if he/she exists:
+		const user = await User.findByPk(req.params.id)
+		if (user) {
+			const [userUpdated] = await User.update(req.body, {
+				where: {
+					id: req.params.id,
+				},
+			})
+
+			if (userUpdated !== 0) {
+				// If we tried to use the variable «user.dataValues» here, 
+				// we would not get the updated values for the user being processed
+				// but their former data:
+				return res.status(200).send('User successfully updated!')
+			} else {
+				// Here, however, we can use the variable as their data has not changed:
+				return res.status(400).json({ message: 'User cannot be updated. +Info: He/She already has those values!', user: user })
+			}
 		} else {
-			return res.status(404).send('User not found')
+			return res.status(404).send('User not found.')
 		}
+
+
 	} catch (error) {
 		return res.status(500).send(error.message)
 	}
@@ -64,9 +81,9 @@ async function deleteUser(req, res) {
 			},
 		})
 		if (user) {
-			return res.status(200).json('User deleted')
+			return res.status(200).json({ message: 'User successfully deleted!', user: user })
 		} else {
-			return res.status(404).send('User not found')
+			return res.status(404).send('User not found.')
 		}
 	} catch (error) {
 		return res.status(500).send(error.message)
