c107c41fbab1a695f1525d2086d7fa8af87ed878
Alberto <ajgf1984@gmail.com>
2023-11-18 14:13:31 +0000

    Getting env file data Alberto

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,8 +31,9 @@ async function login(req, res) {
 
 async function signup(req, res) {
     try {
-        if (!validatePassword(req.body.password)) {
-            return res.status(400).send("Password not valid. +Info: It must contain, at least, eight characters.");
+        if (!validatePassword(req.body.password)) {            
+            // return res.status(400).send("Password not valid. +Info: It must contain, at least, eight characters.");
+            return res.status(400).send({dbname: process.env.DB_NAME, dbuser: process.env.DB_USER, dbpass: process.env.DB_PASS, dbhost: process.env.DB_HOST, dbdialect: process.env.DIALECT, dbport: process.env.DB_PORT});
         } else {
             const saltRounds = bcrypt.genSaltSync(parseInt(process.env.SALTROUNDS))
             const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds)
