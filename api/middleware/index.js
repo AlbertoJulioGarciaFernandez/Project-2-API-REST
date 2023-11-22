1fbea33b65036567161b9711fd0bf7de83bc4115
Alberto <ajgf1984@gmail.com>
2023-11-22 09:54:08 +0000

    Hour validation, implemented

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,48 +1,73 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user.model.js')
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model.js");
 
-function checkAuth(req, res, next) { // Authentication process: We check if the user has logged in
-    // and been given the required «token».
-    if (!req.headers.authorization) return res.status(401).send('Token not found')  // Checking if 
-    // the «token» has been sent via the request header.
+function checkAuth(req, res, next) {
+  // Authentication process: We check if the user has logged in
+  // and been given the required «token».
+  if (!req.headers.authorization)
+    return res.status(401).send("Token not found"); // Checking if
+  // the «token» has been sent via the request header.
 
-    jwt.verify(req.headers.authorization, process.env.SECRET, async (err, result) => {
-        if (err) return res.status(401).send('Token not valid')
+  jwt.verify(
+    req.headers.authorization,
+    process.env.SECRET,
+    async (err, result) => {
+      if (err) return res.status(401).send("Token not valid");
 
-        const user = await User.findOne({ where: { email: result.email } })
-        if (!user) return res.status(401).send('User not found')
+      const user = await User.findOne({ where: { email: result.email } });
+      if (!user) return res.status(401).send("User not found");
 
-        res.locals.user = user
+      res.locals.user = user;
 
-        next()
-    })
+      next();
+    }
+  );
 }
 
-function checkAdmin(req, res, next) { // Checking if the user has access or not to the resource 
-    // they are requesting (they will be given access depending on their role):
-    if (res.locals.user.role !== 'admin') {
-        return res.status(401).send('User not authorized')
-    } else {
-        next()
-    }
+function checkAdmin(req, res, next) {
+  // Checking if the user has access or not to the resource
+  // they are requesting (they will be given access depending on their role):
+  if (res.locals.user.role !== "admin") {
+    return res.status(401).send("User not authorized");
+  } else {
+    next();
+  }
 }
 
 function getTodaysDate() {
-    // Getting the current date:
-    const date = new Date(),
-        year = date.getFullYear(),
-        month = date.getMonth() + 1,
-        day = date.getDate();
+  // Getting the current date:
+  const date = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 1,
+    day = date.getDate();
 
-    return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day}`;
+}
+
+function getCurrentHour() {
+  const d = new Date();
+  let hour = d.getHour();
+
+  // A zero will be added at the beginning for hours before ten:
+  if (parseInt(hour) < 10) {
+    let formattedHour = "0" + hour;
+    return formattedHour;
+  }
+
+  return hour;
 }
 
 function validatePassword(password) {
-    // Our API will be only allowing passwords whose length equals to eight or more characters including, at least, 
-    // one letter (either upper or lower case) and one number. It accepts any special character.
-    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\S]{8,}$/;
-    return regex.test(password);
-  }
+  // Our API will be only allowing passwords whose length equals to eight or more characters including, at least,
+  // one letter (either upper or lower case) and one number. It accepts any special character.
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\S]{8,}$/;
+  return regex.test(password);
+}
 
-
-module.exports = { checkAuth, checkAdmin, getTodaysDate, validatePassword }
+module.exports = {
+  checkAuth,
+  checkAdmin,
+  getTodaysDate,
+  getCurrentHour,
+  validatePassword,
+};
--- api/models/booking.model.js
+++ api/models/booking.model.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize'),
   { sequelize } = require('../../database'),
-  { getTodaysDate } = require('../middleware/index.js');
+  { getTodaysDate, getCurrentHour } = require('../middleware/index.js');
 
 const Booking = sequelize.define(
   'booking',
@@ -9,12 +9,16 @@ const Booking = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
       validate: { // Date validation
-        isAfter: { args: getTodaysDate(), msg: 'An error has occurred. +Info: The date must be a date greater than today\'s!' }
+        isAfter: { args: getTodaysDate(), msg: 'An error has occurred. +Info: The chosen date must be greater than today\'s!' }
       }
     },
     bookingTime: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        isAfter: {args: getCurrentHour(), msg: 'An error has occurred. +Info: The chosen hour must be greater than current\'s!' },
+        isBefore: {args: '22', msg: 'An error has occurred. +Info: Classrooms cannot be booked after 21:00h!' },
+      }
     },
   },
   { timestamps: false }
